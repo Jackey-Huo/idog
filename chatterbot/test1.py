e9cfb19304fdf301015b2e8a53a47e802591e9bc
Jackey-Huo <767327611@qq.com>
2016-12-16 09:30:46 +0800

    starting chartterbot, unfinished

---
+++ chatterbot/test1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from chatterbot import ChatBot
+import logging
+
+
+# Uncomment the following line to enable verbose logging
+# logging.basicConfig(level=logging.INFO)
+
+# Create a new instance of a ChatBot
+bot = ChatBot("Terminal")
+
+print("Type something to begin...")
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        # We pass None to this method because the parameter
+        # is not used by the TerminalAdapter
+        bot_input = bot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
---
+++ chatterbot/test2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot("myBot")
+chatbot.set_trainer(ChatterBotCorpusTrainer)
+
+# 使用英文语料库训练它
+chatbot.train("chatterbot.corpus.english")
+
+# 开始对话
+while True:
+    res = chatbot.get_response(raw_input(">"))
+    print res
